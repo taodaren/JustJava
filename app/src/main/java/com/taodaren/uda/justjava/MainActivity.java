b62a5fc2dd6179656fcf628040122b9d8846b8db
taodaren <taodaren1125@gmail.com>
2017-11-28 15:46:00 +0800

    使用全局变量实现选择器功能

--- app/src/main/java/com/taodaren/uda/justjava/MainActivity.java
+++ app/src/main/java/com/taodaren/uda/justjava/MainActivity.java
@@ -19,6 +19,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +31,16 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 3;
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
     public void increment(View view) {
-        int quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
